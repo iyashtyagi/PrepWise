ca1321bbd5d2a088ff48eb796131a8a4b4f2cba0
Yash Tyagi <contactyashtyagi@gmail.com>
2025-05-19 02:52:21 +0530

    enhanced the feedback with camera details

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -43,15 +43,16 @@ User Camera Details (average over session):
         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
         Transcript:
         ${formattedTranscript}
-        User non-verbal Details:
+        User non-verbal Details: these values varies from 0 to 1(higher is better).
         ${cameraDetailsString}
 
         Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
-        - **Communication Skills**: Clarity, articulation, structured responses.
-        - **Technical Knowledge**: Understanding of key concepts for the role.
-        - **Problem-Solving**: Ability to analyze problems and propose solutions.
-        - **Cultural & Role Fit**: Alignment with company values and job role.
-        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+          -  Communication Skills: Clarity, articulation, structured responses.
+          -  Technical Knowledge: Understanding of key concepts for the role.
+          -  Problem-Solving: Ability to analyze problems and propose solutions.
+          -  Cultural & Role Fit: Alignment with company values and job role.
+          -  Confidence & Clarity: Confidence in responses, engagement, and clarity.
+          -  Non-Verbal Communication: Confidence level, emotional expression, body language.
         `,
       system:
         "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
